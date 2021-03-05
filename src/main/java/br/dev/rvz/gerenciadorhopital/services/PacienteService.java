de19ac98725516b37d6dda81fbce77c7a790120c
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-05 16:02:34 -0300

    criar service de paciente

--- src/main/java/br/dev/rvz/gerenciadorhopital/services/PacienteService.java
+++ src/main/java/br/dev/rvz/gerenciadorhopital/services/PacienteService.java
@@ -1,2 +1,16 @@
-package br.dev.rvz.gerenciadorhopital.services;public class PacienteService {
+package br.dev.rvz.gerenciadorhopital.services;
+
+import br.dev.rvz.gerenciadorhopital.models.Historico;
+import br.dev.rvz.gerenciadorhopital.models.Paciente;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PacienteService {
+
+
+
+
 }
