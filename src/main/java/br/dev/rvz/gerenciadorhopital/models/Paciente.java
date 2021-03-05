de93a3f8f0459d8fee64d3f8def77155a4ae67aa
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-05 15:49:24 -0300

    Criar modelo de paciente

---
+++ src/main/java/br/dev/rvz/gerenciadorhopital/models/Paciente.java
@@ -0,0 +1,45 @@
+package br.dev.rvz.gerenciadorhopital.models;
+
+import java.util.List;
+
+public class Paciente {
+    private String nome;
+    private Integer idade;
+    private String cpf;
+    private List<Historico> historicos;
+
+    public Paciente() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public List<Historico> getHistoricos() {
+        return historicos;
+    }
+
+    public void setHistoricos(List<Historico> historicos) {
+        this.historicos = historicos;
+    }
+}
