3af24242e275fa57b08b07f601914b647762d180
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-05 16:14:40 -0300

    Criar método para cadastar um novo histórico para os historicos do paciente pelo CPF

--- src/main/java/br/dev/rvz/gerenciadorhopital/services/PacienteService.java
+++ src/main/java/br/dev/rvz/gerenciadorhopital/services/PacienteService.java
@@ -2,6 +2,7 @@ package br.dev.rvz.gerenciadorhopital.services;
 
 import br.dev.rvz.gerenciadorhopital.models.Historico;
 import br.dev.rvz.gerenciadorhopital.models.Paciente;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -12,7 +13,10 @@ public class PacienteService {
 
     List<Paciente> pacientes = new ArrayList<>();
 
-    public Paciente cadatrar(Paciente paciente) {
+    @Autowired
+    private HistoricoService historicoService;
+
+    public Paciente cadatrar(Paciente paciente) throws RuntimeException {
         if (!pacientes.contains(paciente)) {
             pacientes.add(paciente);
             return paciente;
@@ -20,5 +24,14 @@ public class PacienteService {
         throw new RuntimeException("O paciente com CPF " + paciente.getCpf() + " Já existe cadastrado");
     }
 
+    public Paciente cadastrarHistorico(String cpf, Historico historico) throws RuntimeException {
+        for (Paciente paciente : pacientes) {
+            if (paciente.getCpf().equals(cpf)) {
+                historicoService.cadastrar(paciente, historico);
+                return paciente;
+            }
+        }
+        throw new RuntimeException("Paciente com CPF " + cpf + " não foi localizado");
+    }
 
 }
