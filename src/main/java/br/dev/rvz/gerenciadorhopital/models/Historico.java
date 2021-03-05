09838bdeeae00badd4936ec990932fcab7e6dd52
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-05 15:50:34 -0300

    Criar modelo de Historico de paciente

---
+++ src/main/java/br/dev/rvz/gerenciadorhopital/models/Historico.java
@@ -0,0 +1,27 @@
+package br.dev.rvz.gerenciadorhopital.models;
+
+import java.time.LocalDate;
+
+public class Historico {
+    private LocalDate dataDoRegistro;
+    private String notaHistorico;
+
+    public Historico() {
+    }
+
+    public LocalDate getDataDoRegistro() {
+        return dataDoRegistro;
+    }
+
+    public void setDataDoRegistro(LocalDate dataDoRegistro) {
+        this.dataDoRegistro = dataDoRegistro;
+    }
+
+    public String getNotaHistorico() {
+        return notaHistorico;
+    }
+
+    public void setNotaHistorico(String notaHistorico) {
+        this.notaHistorico = notaHistorico;
+    }
+}
