83fc440e3200ac11a5beafcf4358507e7e47a6cf
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-05 15:44:32 -0300

    Criar projeto spring boot - gerenciador de pacientes

---
+++ src/main/java/br/dev/rvz/gerenciadorhopital/GerenciadorHopitalApplication.java
@@ -0,0 +1,13 @@
+package br.dev.rvz.gerenciadorhopital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorHopitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorHopitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/rvz/gerenciadorhopital/GerenciadorHopitalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.rvz.gerenciadorhopital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorHopitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
