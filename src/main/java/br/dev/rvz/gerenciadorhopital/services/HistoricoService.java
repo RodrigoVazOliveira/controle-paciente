b421c91c30e1a79ba52fba825d5f4005834a3bd7
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-05 16:05:49 -0300

    Criar método para cadastrar histórico

--- src/main/java/br/dev/rvz/gerenciadorhopital/services/HistoricoService.java
+++ src/main/java/br/dev/rvz/gerenciadorhopital/services/HistoricoService.java
@@ -1,4 +1,12 @@
 package br.dev.rvz.gerenciadorhopital.services;
 
+import br.dev.rvz.gerenciadorhopital.models.Historico;
+import br.dev.rvz.gerenciadorhopital.models.Paciente;
+
 public class HistoricoService {
+
+    public Historico cadastrar(Paciente paciente, Historico historico) {
+        paciente.getHistoricos().add(historico);
+    }
+
 }
