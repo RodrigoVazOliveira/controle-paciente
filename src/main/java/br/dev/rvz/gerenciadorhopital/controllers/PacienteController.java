abd84bd2a4660361c81866b454c8509d8a652f4e
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-05 16:27:37 -0300

    Adicionar o anotation de PUT e resposta de 201 no controlller paciente

--- src/main/java/br/dev/rvz/gerenciadorhopital/controllers/PacienteController.java
+++ src/main/java/br/dev/rvz/gerenciadorhopital/controllers/PacienteController.java
@@ -4,7 +4,9 @@ import br.dev.rvz.gerenciadorhopital.models.Paciente;
 import br.dev.rvz.gerenciadorhopital.services.PacienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -15,6 +17,8 @@ public class PacienteController {
     @Autowired
     private PacienteService pacienteService;
 
+    @PutMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Paciente cadastrarPaciente(Paciente paciente) {
         try {
             pacienteService.cadatrar(paciente);
