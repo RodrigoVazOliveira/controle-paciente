57c668d6015992d958cecd551c5956ad9e0ce63d
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-05 15:53:46 -0300

    Criar modelo de consulta

---
+++ src/main/java/br/dev/rvz/gerenciadorhopital/models/Consulta.java
@@ -0,0 +1,36 @@
+package br.dev.rvz.gerenciadorhopital.models;
+
+import java.time.LocalDate;
+
+public class Consulta {
+    private Paciente paciente;
+    private LocalDate dataConsulta;
+    private TipoConsulta tipoConsulta;
+
+    public Consulta() {
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public LocalDate getDataConsulta() {
+        return dataConsulta;
+    }
+
+    public void setDataConsulta(LocalDate dataConsulta) {
+        this.dataConsulta = dataConsulta;
+    }
+
+    public TipoConsulta getTipoConsulta() {
+        return tipoConsulta;
+    }
+
+    public void setTipoConsulta(TipoConsulta tipoConsulta) {
+        this.tipoConsulta = tipoConsulta;
+    }
+}
