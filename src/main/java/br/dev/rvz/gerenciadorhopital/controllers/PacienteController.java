be0073919115df5fe2c1041f378967e999822cea
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-05 16:26:11 -0300

    Adicionar método de cadastro no controller paciente

--- src/main/java/br/dev/rvz/gerenciadorhopital/controllers/PacienteController.java
+++ src/main/java/br/dev/rvz/gerenciadorhopital/controllers/PacienteController.java
@@ -3,8 +3,10 @@ package br.dev.rvz.gerenciadorhopital.controllers;
 import br.dev.rvz.gerenciadorhopital.models.Paciente;
 import br.dev.rvz.gerenciadorhopital.services.PacienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/paciente/")
@@ -13,6 +15,12 @@ public class PacienteController {
     @Autowired
     private PacienteService pacienteService;
 
-
-
+    public Paciente cadastrarPaciente(Paciente paciente) {
+        try {
+            pacienteService.cadatrar(paciente);
+            return paciente;
+        } catch (RuntimeException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
 }
