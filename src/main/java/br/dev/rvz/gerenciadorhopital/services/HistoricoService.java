50819341cc0a9904dcbe06ec66fc29132998d41c
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-05 16:25:48 -0300

    Adicionar anootation service para classe de servico historico

--- src/main/java/br/dev/rvz/gerenciadorhopital/services/HistoricoService.java
+++ src/main/java/br/dev/rvz/gerenciadorhopital/services/HistoricoService.java
@@ -2,7 +2,9 @@ package br.dev.rvz.gerenciadorhopital.services;
 
 import br.dev.rvz.gerenciadorhopital.models.Historico;
 import br.dev.rvz.gerenciadorhopital.models.Paciente;
+import org.springframework.stereotype.Service;
 
+@Service
 public class HistoricoService {
 
     public Historico cadastrar(Paciente paciente, Historico historico) {
