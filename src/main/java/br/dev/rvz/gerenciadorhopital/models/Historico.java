0770b3fef9dc6e58db4a3cfe6f8059eaa64117ef
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-08 16:53:39 -0300

    Adicionar organização por data do historico

--- src/main/java/br/dev/rvz/gerenciadorhopital/models/Historico.java
+++ src/main/java/br/dev/rvz/gerenciadorhopital/models/Historico.java
@@ -2,7 +2,7 @@ package br.dev.rvz.gerenciadorhopital.models;
 
 import java.time.LocalDate;
 
-public class Historico {
+public class Historico implements Comparable<Historico> {
     private LocalDate dataDoRegistro;
     private String notaHistorico;
 
@@ -23,5 +23,10 @@ public class Historico {
 
     public void setNotaHistorico(String notaHistorico) {
         this.notaHistorico = notaHistorico;
+    }
+
+    @Override
+    public int compareTo(Historico o) {
+        return getDataDoRegistro().compareTo(o.getDataDoRegistro());
     }
 }
--- src/main/java/br/dev/rvz/gerenciadorhopital/services/HistoricoService.java
+++ src/main/java/br/dev/rvz/gerenciadorhopital/services/HistoricoService.java
@@ -4,11 +4,14 @@ import br.dev.rvz.gerenciadorhopital.models.Historico;
 import br.dev.rvz.gerenciadorhopital.models.Paciente;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+
 @Service
 public class HistoricoService {
 
     public Historico cadastrar(Paciente paciente, Historico historico) {
         paciente.getHistoricos().add(historico);
+        Collections.sort(paciente.getHistoricos());
         return historico;
     }
 
