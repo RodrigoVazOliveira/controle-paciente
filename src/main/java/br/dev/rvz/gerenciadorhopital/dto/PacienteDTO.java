c39893748d40aaa6824e468dc52544cf2fe5d768
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-08 10:27:23 -0300

    Criar DTO de paciente para não exibir o historico na pesquisa de consulta

---
+++ src/main/java/br/dev/rvz/gerenciadorhopital/dto/PacienteDTO.java
@@ -0,0 +1,34 @@
+package br.dev.rvz.gerenciadorhopital.dto;
+
+public class PacienteDTO {
+    private String nome;
+    private Integer idade;
+    private String cpf;
+
+    public PacienteDTO() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
