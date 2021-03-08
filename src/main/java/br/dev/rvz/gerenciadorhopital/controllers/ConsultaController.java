8dcb51dd6327fb2d55aa2af6b26944f4990c49af
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-08 10:12:05 -0300

    Criar método de cadastrar no controller de consulta

--- src/main/java/br/dev/rvz/gerenciadorhopital/controllers/ConsultaController.java
+++ src/main/java/br/dev/rvz/gerenciadorhopital/controllers/ConsultaController.java
@@ -1,9 +1,27 @@
 package br.dev.rvz.gerenciadorhopital.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.dev.rvz.gerenciadorhopital.dto.ConsultaDTO;
+import br.dev.rvz.gerenciadorhopital.models.Consulta;
+import br.dev.rvz.gerenciadorhopital.services.ConsultaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/consulta/")
 public class ConsultaController {
+
+    @Autowired
+    private ConsultaService service;
+
+    @PutMapping("cadastrar")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Consulta cadastrar(@RequestBody ConsultaDTO consulta, @RequestParam("cpf") String cpf) {
+        try {
+            return service.cadastrar(consulta, cpf);
+        } catch (RuntimeException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
 }
