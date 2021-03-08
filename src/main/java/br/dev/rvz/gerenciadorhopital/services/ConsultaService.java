ca5c2c23838eacc937e115444f0f6afade71fa30
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-08 10:31:55 -0300

    Criar método para converter consulta para consulta dto no serviço de consulta'

--- src/main/java/br/dev/rvz/gerenciadorhopital/services/ConsultaService.java
+++ src/main/java/br/dev/rvz/gerenciadorhopital/services/ConsultaService.java
@@ -1,6 +1,7 @@
 package br.dev.rvz.gerenciadorhopital.services;
 
 import br.dev.rvz.gerenciadorhopital.dto.ConsultaDTO;
+import br.dev.rvz.gerenciadorhopital.dto.PacienteDTO;
 import br.dev.rvz.gerenciadorhopital.models.Consulta;
 import br.dev.rvz.gerenciadorhopital.models.Paciente;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,6 +45,21 @@ public class ConsultaService {
         }
 
         throw new RuntimeException("Não foi localizado nenhuma consulta com a data " + dataConsulta);
+    }
+
+    private ConsultaDTO converterConsultaParaConsultaDTO(Consulta consulta) {
+        ConsultaDTO consultaDTO = new ConsultaDTO();
+        consultaDTO.setDataConsulta(consulta.getDataConsulta());
+        consultaDTO.setTipoConsulta(consulta.getTipoConsulta());
+
+        PacienteDTO pacienteDTO = new PacienteDTO();
+        pacienteDTO.setNome(consulta.getPaciente().getNome());
+        pacienteDTO.setCpf(consulta.getPaciente().getCpf());
+        pacienteDTO.setIdade(consulta.getPaciente().getIdade());
+
+        consultaDTO.setPacienteDTO(pacienteDTO);
+
+        return consultaDTO;
     }
 
     public void excluirConsulta(Consulta consulta) {
