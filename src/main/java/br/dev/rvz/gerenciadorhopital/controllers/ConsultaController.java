b3b5bedf8f05b5ef7040c75c191feb89d439ad83
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-08 15:50:16 -0300

    REfatorar método cadastrar a consulta com DTO

--- src/main/java/br/dev/rvz/gerenciadorhopital/controllers/ConsultaController.java
+++ src/main/java/br/dev/rvz/gerenciadorhopital/controllers/ConsultaController.java
@@ -1,8 +1,11 @@
 package br.dev.rvz.gerenciadorhopital.controllers;
 
 import br.dev.rvz.gerenciadorhopital.dto.ConsultaDTO;
+import br.dev.rvz.gerenciadorhopital.dto.PacienteDTO;
 import br.dev.rvz.gerenciadorhopital.models.Consulta;
+import br.dev.rvz.gerenciadorhopital.models.Paciente;
 import br.dev.rvz.gerenciadorhopital.services.ConsultaService;
+import br.dev.rvz.gerenciadorhopital.services.PacienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,11 +23,16 @@ public class ConsultaController {
     @Autowired
     private ConsultaService service;
 
+    @Autowired
+    private PacienteService pacienteService;
+
     @PutMapping("cadastrar")
     @ResponseStatus(HttpStatus.CREATED)
-    public Consulta cadastrar(@RequestBody ConsultaDTO consulta, @RequestParam("cpf") String cpf) {
+    public Consulta cadastrar(@RequestBody ConsultaDTO consultaDTO, @RequestParam("cpf") String cpf) {
         try {
-            return service.cadastrar(consulta, cpf);
+            Paciente paciente = pacienteService.pesquisar(cpf);
+            Consulta consulta = consultaDTO.converterConsultaDTOParaConsulta(consultaDTO, paciente);
+            return service.cadastrar(consulta);
         } catch (RuntimeException e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
         }
@@ -36,7 +45,12 @@ public class ConsultaController {
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
                     LocalDate dataDaConsulta) {
         try {
-            return service.pesquisarConsultaPorData(dataDaConsulta);
+            List<Consulta> dados = service.pesquisarConsultaPorData(dataDaConsulta);
+            List<ConsultaDTO> consultaDTOS = new ArrayList<>();
+            for (Consulta consulta : dados) {
+                consultaDTOS.add(new ConsultaDTO().converterConsultaParaConsultaDTO(consulta));
+            }
+            return consultaDTOS;
         } catch (RuntimeException e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
         }
